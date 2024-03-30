3b00315820bc4a076e6a75e85f6ef54a7eb39dae
Sean Ryan <sfryan95@gmail.com>
2024-03-29 20:30:46 -0700

    added user controller and user route handler; added functionality to insert coin data into portfolio table; updated cryptoform to send requests to server for both api fetch and entry into database; added redirect upon successful login

--- server/dbSchemas/usersSchema.js
+++ server/dbSchemas/usersSchema.js
@@ -3,7 +3,6 @@ import pool from '../database.js';
 const createUsersTable = async () => {
   const queryText = `CREATE TABLE users (
       id SERIAL PRIMARY KEY, 
-      username VARCHAR(50) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL,
       email VARCHAR(255) UNIQUE NOT NULL
     );`;
